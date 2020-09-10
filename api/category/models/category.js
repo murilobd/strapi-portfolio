bf61a615363c01c48315a437bb0488d31841ebcd
Murilo Boareto Delefrate <murilobd@gmail.com>
2020-09-10 19:40:00 +0200

    - added slug to categories

--- api/category/models/category.js
+++ api/category/models/category.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
+const slugify = require("slugify");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+function createSlug(str) {
+  return slugify(str.toLowerCase().trim());
+}
+
+module.exports = {
+  lifecycles: {
+    beforeCreate: async (data) => {
+      if (data.Title) {
+        data.slug = createSlug(data.Title);
+      }
+    },
+    beforeUpdate: async (params, data) => {
+      data.slug = createSlug(data.Title);
+    },
+  },
+};
